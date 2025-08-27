7c2d64112c5734616ad6dbd0a3a19e88cfb03897
VinihAugs <vini1542009@gmail.com>
2025-08-27 10:14:46 -0300

    app green lantern2

--- App.tsx
+++ App.tsx
@@ -1,20 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity, StatusBar } from 'react-native';
+import symbolOn from './assets/pictures/symbolon.png';
+import symbolOff from './assets/pictures/symboloff.png';
 
 export default function App() {
+  const [isActive, setIsActive] = useState(false);
+
+  const textLight = 'No dia mais claro, na noite mais escura, nenhum mal escapará da minha vista.';
+  const textShadow = 'Que aqueles que adoram o mal tremam diante do poder do Lanterna Verde!';
+
+  function handleSymbol() {
+    setIsActive(currentValue => !currentValue);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={isActive ? styles.containerOn : styles.containerOff}>
+      
+      <StatusBar
+        backgroundColor={isActive ? '#057c05' : '#000000'} 
+        barStyle={isActive ? 'light-content' : 'dark-content'} 
+      />
+
+      <TouchableOpacity onPress={handleSymbol} style={styles.button}>
+        
+        <Image 
+          style={styles.logo}
+          source={isActive ? symbolOn : symbolOff}
+        />
+        
+        <Text style={isActive ? styles.textOn : styles.textOff}>
+          {isActive ? textShadow : textLight}
+        </Text>
+
+      </TouchableOpacity>
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
-  container: {
+  containerOn: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal: 20, 
   },
+  containerOff:{
+    flex: 1,
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+  },
+  button: {
+    alignItems: 'center', 
+  },
+  logo: {
+    width: 200,
+    height: 200,
+    resizeMode: 'contain',
+    marginBottom: 30,
+  },
+  textOn: {
+    color: '#057c05', 
+    textAlign: 'center',
+    fontSize: 20,
+  },
+  textOff: {
+    color: 'black', 
+    textAlign: 'center',
+    fontSize: 20,
+  }
 });
